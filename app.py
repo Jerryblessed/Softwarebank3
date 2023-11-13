2b1177539f9e63e91fd43a103e84a5dd5e2be26b
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2023-11-14 00:00:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index2.html')  # Render a template
 
 if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
+    port = os.environ.get('FLASK_PORT') or 5000  # Change to the port you prefer
     port = int(port)
 
-    app.run(port=port,host='0.0.0.0')
+    app.run(port=port, host='0.0.0.0')
